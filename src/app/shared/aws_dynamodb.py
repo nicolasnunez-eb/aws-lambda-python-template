7a5dd28d29f220c9ae2b8d0e019f7a6f635c0464
nicolas.nunez <nicolas.nunez@eventbrite.com>
2022-08-14 09:41:19 -0300

    Fix pipfile and add pipfile.lock

---
+++ src/app/shared/aws_dynamodb.py
@@ -0,0 +1,33 @@
+from typing import Generic, Literal, Mapping, Optional, TypeVar, Union, cast
+
+import boto3
+from mypy_boto3_dynamodb.service_resource import DynamoDBServiceResource
+
+session = boto3.Session()
+dynamodb_resource = session.resource("dynamodb")
+
+
+Table = Literal["OnboardingUserResponses"]
+
+T = TypeVar("T", bound=Mapping)
+
+
+class DynamoDBClient(Generic[T]):
+    dynamo_resource: DynamoDBServiceResource
+
+    def __init__(self, dynamo_resource: DynamoDBServiceResource, table_name: Table):
+        self.dynamo_resource = dynamo_resource
+        self.table = self.dynamo_resource.Table(table_name)
+
+    def store(self, entity: T) -> None:
+        """
+        raises : botocore.exceptions.ClientError
+        """
+        self.table.put_item(Item=entity)
+
+    def find_by_id(self, id: Union[str, int]) -> Optional[T]:
+        try:
+            item = self.table.get_item(Key={"organization_id": id})
+            return cast(T, item["Item"])
+        except self.dynamo_resource.meta.client.exceptions.ResourceNotFoundException:
+            return None
